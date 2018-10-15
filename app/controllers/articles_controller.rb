2b7b06e4ac3585aee9e6f0d56006c6181ce766e6
Ann Adaya <ryry11adaya@gmail.com>
2018-10-15 05:58:23 +0000

    Fix create, add flash and error messages, create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,11 +18,36 @@ class ArticlesController < ApplicationController
         
         # render plain: params[:article].inspect
 
-        @article = Article.new(article_params)
-        @article.save
+# we unquote this because we still need to create article with the article params
+     @article = Article.new(article_params)
+     
+        # @article.save
+        # redirect_to article_path(@article)
+        #redirect_to articles_show(@article) - and this is wrong, wrong path. 
+
+#now we have to handle displaying this flash notice - so where do we display it? 
+#basically all the views are wrapped within application.html.erb, basically all the views, this is a wrapper file. So anything we put in here will be displayed on every page, so we want our flash notice to be displayed here - views-layouts-applications.
+
+# now what if validations happened here? When we dont put any data and just clicked create article, notice that it simply refreshes because of our condition - else new - but does not show any message about error. So in order to do that, we would want to check it here.
+
+
+        if @article.save
+            flash[:notice] = "Article was successfully created"
+            redirect_to article_path(@article)
+            
+        else 
+            render 'new' #validation not accepted, try again
+            
+        end    
+    end
+    
+# this is the action to show the pages of the blogs created using ids
+    def show
+        @article = Article.find(params[:id])
         
-        end
-        
+    end
+    
+    
         private
         	def article_params
         	
